3094acb1d7f764ae4356a031f0843e38ff0a73ec
wpena-87 <walgupen@espol.edu.ec>
2021-02-13 03:05:19 -0500

    [ADD] InGame Scene, Background and Cards

---
+++ Assets/Scripts/CardsHandlerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsHandlerController : MonoBehaviour
+{
+    public GameObject cardPrefab;
+
+    
+    void Start()
+    {
+        int x = -357;
+        int y =  198;
+        for (int i = 0; i < 8; i++)
+        {
+            Instantiate(cardPrefab, new Vector3(x, y, 0), new Quaternion());
+            x += 200;
+            if (i == 3)
+            {
+                x = -357;
+                y = - 52;
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
