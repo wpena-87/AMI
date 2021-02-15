49dc0f850eb8fb4afdc87d0449ec01cbfa071619
wpena-87 <walgupen@espol.edu.ec>
2021-02-15 17:22:07 -0500

    [ADD] Matching functionality

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    Sprite face;
+    Sprite back;
+    SpriteRenderer spriteRenderer;
+
+    bool matched = false;
+
+    static List<Card> shownCards = new List<Card>();
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void OnMouseDown()
+    {
+        if (matched)
+        {
+            return;
+        }
+        
+        if (shownCards.Contains(this))
+        {
+            spriteRenderer.sprite = back;
+            shownCards.Remove(this);
+            return;
+        }
+
+        if (shownCards.Count < 2)
+        {
+            spriteRenderer.sprite = face;
+            shownCards.Add(this);
+        }
+        if (shownCards.Count == 2 && shownCards[0].face == shownCards[1].face)
+        {
+            shownCards[0].matched = true;
+            shownCards[1].matched = true;
+            shownCards.Clear();
+        }
+    }
+
+    public void setFace(Sprite face)
+    {
+        this.face = face;
+    }
+
+    public void setBack(Sprite back)
+    {
+        this.back = back;
+    }
+}
---
+++ Assets/Scripts/CardsManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsManager : MonoBehaviour
+{
+    public GameObject Card;
+    public Sprite[] cardFaces;
+    public Sprite cardBack;
+    static System.Random random = new System.Random();
+    
+    void Start()
+    {
+        PlaceCards();
+    }
+
+    private List<Sprite> GetShuffleFaces()
+    {
+        List<Sprite> shuffledFaces = new List<Sprite>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            Sprite face = cardFaces[random.Next(cardFaces.Length)];
+            while (shuffledFaces.Contains(face))
+            {
+                face = cardFaces[random.Next(cardFaces.Length)];
+            }
+            shuffledFaces.Add(face);
+            shuffledFaces.Insert(random.Next(shuffledFaces.Count), face);
+        }
+        
+        return shuffledFaces;
+    }
+
+    private void PlaceCards()
+    {
+         List<Sprite> shuffledFaces = GetShuffleFaces();
+
+        int x = -357;
+        int y =  198;
+        
+        int k = 0;
+        foreach (Sprite cardFace in shuffledFaces)
+        {
+            GameObject card = Instantiate(Card, new Vector3(x, y, 0), Quaternion.identity);
+            card.GetComponent<Card>().setFace(cardFace);
+            card.GetComponent<Card>().setBack(cardBack);
+            x += 200;
+            if (k == 3)
+            {
+                x = -357;
+                y = - 52;
+            }
+            k++;
+        }
+    }
+
+}
