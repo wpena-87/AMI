143cbde1aa5e40e424cec17da35f44f43c0dceaa
wpena-87 <walgupen@espol.edu.ec>
2021-02-16 00:56:32 -0500

    [ADD] Game over assets & animations

---
+++ Assets/Scripts/BehaviorManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BehaviorManager : MonoBehaviour
+{
+    public Text label;
+    public Image logo;
+    public Sprite win;
+    public Sprite loss;
+
+    void Start()
+    {
+        if (ScoreManager.GetScore() == 100)
+        {
+            label.text = "You Won!";
+            logo.sprite = win;
+        }
+        else
+        {
+            label.text = "You Lost!";
+            logo.sprite = loss;
+            logo.rectTransform.position += new Vector3(0, 100, 0);
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 xSpeed = new Vector3(1000 * Time.deltaTime, 0, 0);
+        if (logo.GetComponent<RectTransform>().position.x < 512)
+        {
+            logo.GetComponent<RectTransform>().position += xSpeed;
+            label.GetComponent<RectTransform>().position -= xSpeed;
+        }
+        else
+        {
+
+        }
+    }
+
+    void Update()
+    {
+        Move();
+    }
+}
