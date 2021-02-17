7c37b50fdfe2f829cc980f11a20fe8191abf7696
wpena-87 <walgupen@espol.edu.ec>
2021-02-17 00:11:53 -0500

    [MOD] GameOver scene minor parameters

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BehaviorManager : MonoBehaviour
+public class GameOverManager : MonoBehaviour
 {
     public Text label;
     public Image logo;
