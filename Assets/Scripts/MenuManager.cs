00b3535f874446dc071e5646dd56c59b1652396b
wpena-87 <walgupen@espol.edu.ec>
2021-02-17 03:01:04 -0500

    [ADD] Menu scene & animations

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] leftCards;
+    public GameObject[] rightCards;
+    public Button startButton;
+    int x = -357;
+    float timeCounter = 0;
+    readonly float SPEED = 1000;
+    enum State{
+        IN,
+        OUT,
+        WAIT,
+        END
+    }
+    State state = State.IN;
+
+    private void OnStartButtonClick()
+    {
+        state = State.OUT;
+        startButton.onClick.RemoveAllListeners();
+    }
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnStartButtonClick);
+    }
+
+    private void MoveCards()
+    {
+        Vector3 xVelocity = new Vector3(SPEED, 0, 0) * Time.deltaTime;
+        for (int i = 0; i < 4; i++)
+        {
+            leftCards[i].transform.position += xVelocity;
+            rightCards[i].transform.position -= xVelocity;
+        }
+    }
+
+    private void MoveButton(int dir)
+    {
+        Vector3 yVelocity = new Vector3(0, SPEED, 0) * Time.deltaTime;
+        startButton.transform.position += dir * yVelocity;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case State.IN:
+                MoveCards();
+                MoveButton(1);
+                if (leftCards[0].transform.position.x >= x)
+                {
+                    state = State.WAIT;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        Vector3 pos = leftCards[i].transform.position;
+                        pos.x = x;
+                        leftCards[i].transform.position = pos;
+
+                        pos = rightCards[i].transform.position;
+                        pos.x = x;
+                        rightCards[i].transform.position = pos;
+
+                        pos = startButton.transform.localPosition;
+                        pos.y = -250;
+                        startButton.transform.localPosition = pos;
+
+                        x += 200;
+                    }
+                }
+                break;
+            case State.WAIT:
+                timeCounter += Time.deltaTime;
+                if (timeCounter < 0.5)
+                {
+                    Color buttonColor = startButton.image.color;
+                    buttonColor.a -= Time.deltaTime;
+                    startButton.image.color = buttonColor;
+                }
+                else if (timeCounter < 1)
+                {
+                    Color buttonColor = startButton.image.color;
+                    buttonColor.a += Time.deltaTime;
+                    startButton.image.color = buttonColor;
+                }
+                else
+                {
+                    timeCounter = 0;
+                }
+                break;
+            case State.OUT:
+                MoveCards();
+                MoveButton(-1);
+                if (rightCards[0].transform.position.x <= -1600)
+                {
+                    state = State.END;
+                }
+                break;
+            case State.END:
+                SceneManager.LoadScene("InGame");
+                break;
+        }
+    }
+}
