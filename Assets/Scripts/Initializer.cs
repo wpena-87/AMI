fc53765f784a29ddb43ed6b31fcf799163b9a0bc
wpena-87 <walgupen@espol.edu.ec>
2021-02-18 03:40:49 -0500

    [ADD] Init scene to avoid bug in Chromium based browsers

---
+++ Assets/Scripts/Initializer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Initializer : MonoBehaviour
+{
+    public Text label;
+    float timeCounter = 0;
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    void Update()
+    {
+        Color color = label.color;
+        timeCounter += Time.deltaTime;
+        if (timeCounter < 0.75)
+        {
+            color.a -= Time.deltaTime;
+        }
+        else if (timeCounter < 1.5)
+        {
+            color.a += Time.deltaTime;
+        }
+        else
+        {
+            timeCounter = 0;
+        }
+        label.color = color;
+    }
+}
